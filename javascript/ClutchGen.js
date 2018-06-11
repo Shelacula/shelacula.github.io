e71eef3489810976c7431dbe72567a6c1b8f9bda
Shelacula <40150200+Shelacula@users.noreply.github.com>
2018-06-11 00:06:26 -0400

    Update ClutchGen.js

--- javascript/ClutchGen.js
+++ javascript/ClutchGen.js
@@ -1,19 +1,49 @@
 	function statGen() {
 
+	var greencheck = document.getElementById("greencheck");
+	var goldcheck = document.getElementById("goldcheck");
+	
 	var bronze = Math.floor((Math.random() * 3) + 1);
 	var brown = Math.floor((Math.random() * 4) + 2);
 	var blue = Math.floor((Math.random() * 6) + 4);
 	var green = Math.floor((Math.random() * 7) + 10);
 	var clutchsize = Math.floor(bronze + brown + blue + green)
 	
-	var size = document.getElementById("clutchsize");
-	size.innerHTML = clutchsize;
-	var bronzes = document.getElementById("bronzes");
-	bronzes.innerHTML = bronze;
-	var browns = document.getElementById("browns");
-	browns.innerHTML = brown;
-	var blues = document.getElementById("blues");
-	blues.innerHTML = blue;
-	var greens = document.getElementById("greens");
-	greens.innerHTML = green;
-	}
+	var greenbronze = Math.floor((Math.random() * 1) + 0);
+	var greenbrown = Math.floor((Math.random() * 2) + 0);
+	var greenblue = Math.floor((Math.random() * 3) + 1);
+	var greengreen = Math.floor((Math.random() * 2) + 3);
+	var greenclutchsize = Math.floor(greenbronze + greenbrown + greenblue + greengreen)
+	
+	var goldbronze = Math.floor((Math.random() * 3) + 0);
+	var goldbrown = Math.floor((Math.random() * 3) + 0);
+	var goldblue = Math.floor((Math.random() * 3) + 1);
+	var goldgreen = Math.floor((Math.random() * 3) + 3);
+	var goldchance = Math.round(Math.random() * 100)
+	var goldclutchsize = Math.floor(goldbronze + goldbrown + goldblue + goldgreen)
+
+if (greencheck.checked) {
+	document.getElementById("clutchsize").innerHTML = greenclutchsize;
+	document.getElementById("bronzes").innerHTML = greenbronze;
+	document.getElementById("browns").innerHTML = greenbrown;
+	document.getElementById("blues").innerHTML = greenblue;
+	document.getElementById("greens").innerHTML = greengreen;
+} else if (goldcheck.checked) {
+	document.getElementById("clutchsize").innerHTML = goldclutchsize;
+	document.getElementById("bronzes").innerHTML = goldbronze;
+	document.getElementById("browns").innerHTML = goldbrown;
+	document.getElementById("blues").innerHTML = goldblue;
+	document.getElementById("greens").innerHTML = goldgreen;
+	if (goldbronze < 2 && goldchance > 70) {
+	document.getElementById("gold").innerHTML = "1";
+	} else {
+	document.getElementById("gold").innerHTML = " ";
+}
+} else {
+	document.getElementById("clutchsize").innerHTML = clutchsize;
+	document.getElementById("bronzes").innerHTML = bronze;
+	document.getElementById("browns").innerHTML = brown;
+	document.getElementById("blues").innerHTML = blue;
+	document.getElementById("greens").innerHTML = green;
+}
+}
